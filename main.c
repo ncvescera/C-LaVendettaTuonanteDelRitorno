c6a52a3aae2decbdc9350983e79800c9db58347b
Nicolò Vescera <slayercandy@icloud.com>
2015-10-06 20:09:28 +0200

    Creato con successo record e file funzionante

--- main.c
+++ main.c
@@ -22,8 +22,10 @@ int main(){
     char docente[MAXSTRLEN+1];
     */
     int i = 0;
+    int cont;
     record *array;
-    char temp[MAXSTRLEN+1];
+    FILE *puntafileTemp;
+    char temp[MAXSTRLEN+50];
     FILE *puntafileRuoli;
     
     puntafileRuoli = fopen("ruoli.txt","r");
@@ -33,6 +35,7 @@ int main(){
     }
     
     array = (record*)malloc(i*sizeof(record));
+    fseek(puntafileRuoli,0,SEEK_SET); //si scosta di 0 pisizioni dall'inizio del file (SEEK_SET è l'inizio del file)
     i=0;
     while(!feof(puntafileRuoli)){
         fscanf(puntafileRuoli,"%s\n",array[i].ruolo);
@@ -40,21 +43,20 @@ int main(){
         i++;
     }
     printf("%s,%s,%s",array[0].ruolo,array[1].ruolo,array[2].ruolo);
-    /*
-    FILE *puntafileDOCENTE;
-    puntafileDOCENTE = fopen("docente.txt","r");
-    fscanf(puntafileDOCENTE,"%s",docente);
-    replace(docente,'+',' ');
     
-    FILE *puntafileDIRIGENTE;
-    puntafileDIRIGENTE = fopen("dirigente.txt","r");
-    fscanf(puntafileDIRIGENTE,"%s",dirigente);
-    replace(dirigente,'+',' ');
+    cont = i;
+    i = 0;
+    while(cont>0){
+        strcat(array[i].ruolo,".txt");
+        puntafileTemp = fopen(array[i].ruolo,"r");
+        fscanf(puntafileTemp,"%s",array[i].dati);
+        replace(array[i].dati,'+',' ');
+        fclose(puntafileTemp);
+        cont --;
+        i++;
+    }
     
-    FILE *puntafileSTUDENTE;
-    puntafileSTUDENTE = fopen("studente.txt","r");
-    fscanf(puntafileSTUDENTE,"%s",studente);
-    replace(studente,'+',' ');
+    printf("\n\n%s",array[1].dati);
     
     FILE *puntafileHTML;
     puntafileHTML = fopen("index.html","w");
@@ -65,12 +67,17 @@ int main(){
     substring(stringa,ruolo);
      */
     //fprintf(puntafileHTML,"Content-type:text\n\n");
-    /*
+    
     fprintf(puntafileHTML,"<html>\n");
     fprintf(puntafileHTML,"<head><title>LaPaginaPseudodinamica</title></head>");
     fprintf(puntafileHTML,"<body>\n");
     //form
-    fprintf(puntafileHTML,"<form name=\"frm\"><center><h1>Seleziona il campo da stampare</h1><br><table><tr><td width=\"320px\"><select name=\"sel\" class=\"form-control\" style=\"width:300px;\" onchange=\"aggiornaHidden(this)\"><option value=\"\"  selected=\"selected\">Seleziona</option><option value=\"<h1>Docente</h1>%s\">Docente</option><option value=\"<h1>Dirigente</h1>%s\">Dirigente</option><option value=\"<h1>Studente</h1>%s\">Studente</option></select></td><td></td></tr></table></form>",docente,dirigente,studente);
+    fprintf(puntafileHTML,"<form name=\"frm\"><center><h1>Seleziona il campo da stampare</h1><br><table><tr><td width=\"320px\"><select name=\"sel\" class=\"form-control\" style=\"width:300px;\" onchange=\"aggiornaHidden(this)\"><option value=\"\"  selected=\"selected\">Seleziona</option>");
+    cont = i;
+    for(i=0;i<cont;i++)
+        fprintf(puntafileHTML,"<option value=\"<h1>%s</h1>%s\">%s</option>",array[i].ruolo,array[i].dati,array[i].ruolo);
+    
+    fprintf(puntafileHTML,"</select></td></tr></talbe></form>");
     //scriptJS
     fprintf(puntafileHTML,"<SCRIPT type=\"text/javascript\">\nfunction aggiornaHidden(sel){var f = document.frm;f.value = sel.options[sel.selectedIndex].value; ; document.write(\"<center>\"+f.value+\"</center>\");}</SCRIPT>");
     
@@ -78,7 +85,7 @@ int main(){
     fprintf(puntafileHTML,"</center></body>\n");
     fprintf(puntafileHTML,"</html>\n");
     fclose(puntafileHTML);
-    */
+    
     return(EXIT_SUCCESS);
 }
 void substring(char stringa[], char ruolo[]){
