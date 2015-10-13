c04466b2e2229afb53f20d88eeafef97d7cd48aa
Nicolò Vescera <slayercandy@icloud.com>
2015-10-13 08:50:11 +0200

    Update main.c

--- main.c
+++ main.c
@@ -84,7 +84,7 @@ int main(){
     fprintf(puntafileHTML,"<head><title>LaPaginaPseudodinamica</title></head>");
     fprintf(puntafileHTML,"<body>\n");
     //form
-    fprintf(puntafileHTML,"<form name=\"frm\"><center>\n<h1>Seleziona il campo da stampare</h1><br>\n<table><tr><td width=\"320px\">\n<select id=\"select\" name=\"sel\" class=\"form-control\" style=\"width:373px;\" autofocus onchange=\"printNames()\">\n\t<option value=\"\"  selected=\"selected\">Seleziona</option>\n");
+    fprintf(puntafileHTML,"<center><form name=\"frm\">\n<h1>Seleziona il campo da stampare</h1><br>\n<table><tr><td width=\"320px\">\n<select id=\"select\" name=\"sel\" class=\"form-control\" style=\"width:373px;\" autofocus onchange=\"printNames()\">\n\t<option value=\"\"  selected=\"selected\">Seleziona</option>\n");
     
     //scrive le varie option 
     cont = i;
@@ -92,18 +92,19 @@ int main(){
         fprintf(puntafileHTML,"\t<option value=\'[");
         k = array[i].elementi[i].lunghezza;
         j = 0;
-        while(k-1){
+        while(k-2){
             fprintf(puntafileHTML,"\"%s\",",array[i].elementi[j].studente);
             k--;
             j++;
         }
+        fprintf(puntafileHTML,"\"%s\"",array[i].elementi[j].studente);
         fprintf(puntafileHTML,"]\'>%s</option>\n",array[i].ruolo);
     }
-    fprintf(puntafileHTML,"</select>\n</td>\n</tr>\n</table>\n</center>\n</form> ");
+    fprintf(puntafileHTML,"</select>\n</td>\n</tr>\n</table>\n</form> ");
     
     //scriptJS
-    fprintf(puntafileHTML,"<SCRIPT type=\"text/javascript\">\nfunction printNames(){\nvar e = document.getElementById(\'select\');\nvar array = JSON.parse(e.options[e.selectedIndex].value);\narray = array.sort();\nvar i = 0;document.getElementById(\'prova\').innerHTML = \"\";\nfor(i=0;i<array.length;i++){document.getElementById(\'prova\').innerHTML += array[i]+\"<br>\";}\n}\n</SCRIPT>\n");
-    fprintf(puntafileHTML,"<div id=\"prova\"></div>"); 
+    fprintf(puntafileHTML,"<SCRIPT type=\"text/javascript\">\nfunction printNames(){\nvar e = document.getElementById(\'select\');\nvar array = JSON.parse(e.options[e.selectedIndex].value);\nvar ruolo = e.options[e.selectedIndex].text;\narray = array.sort();\nvar i = 0;document.getElementById(\'prova\').innerHTML = \"\";\ndocument.getElementById(\'prova\').innerHTML=\"<h1>\"+ruolo+\"</h1>\";\nfor(i=0;i<array.length;i++){document.getElementById(\'prova\').innerHTML += array[i]+\"<br>\";}\n}\n</SCRIPT>\n");
+    fprintf(puntafileHTML,"<div id=\"prova\"></div></center>"); 
     fprintf(puntafileHTML,"</body>\n");
     fprintf(puntafileHTML,"</html>\n");
     
